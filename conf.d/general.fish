34a7d1293c4e55cab40b23ce0f828c5a0852d29c
Marschner, Christoffer <christoffer.marschner@sew-eurodrive.de>
2024-11-22 11:40:48 +0100

    added powershell abbreviation to ignored os-specific.fish

--- conf.d/general.fish
+++ conf.d/general.fish
@@ -3,12 +3,6 @@
 abbr --add so "source /home/chris/.config/**/*.fish"
 # zellij
 abbr --add z "zellij"
-# powershell
-abbr --add p "powershell.exe"
-# powershell git commands
-abbr --add pgf "powershell.exe git fetch"
-abbr --add pgps "powershell.exe git push"
-abbr --add pgpl "powershell.exe git pull"
 # custom cd function for calling ls at end
 function c
   cd $argv;
