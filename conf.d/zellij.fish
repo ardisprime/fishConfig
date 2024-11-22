a999544e64c66e7c7cffb64618b7e3d445111b4f
Marschner, Christoffer <christoffer.marschner@sew-eurodrive.de>
2024-11-22 12:22:02 +0100

    fixed type in zellij

--- conf.d/zellij.fish
+++ conf.d/zellij.fish
@@ -3,6 +3,6 @@ abbr --add z "zellij"
 abbr --add za "zellij attach"
 abbr --add zl "zellij list-sessions"
 abbr --add zk "zellij kill-session"
-abbr --add zka "zellij kill-all-session"
+abbr --add zka "zellij kill-all-sessions"
 abbr --add zd "zellij delete-all-sessions"
 
