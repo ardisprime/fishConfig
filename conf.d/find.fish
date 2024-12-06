183a52e2666c52cfb06f2306c529c8d7af9bc4bc
Christoffer, Marschner <christoffer.marschner@sew-eurodrive.de>
2024-12-06 09:12:50 +0100

    fixed find commands and added find for excluding git ignores

--- conf.d/find.fish
+++ conf.d/find.fish
@@ -1,10 +1,13 @@
 
 # functions
 function find-directories
-  c $(fd -I -t d . ($argv | string collect) | fzf)
+  c $(fd -I -t d | fzf)
+end
+function find-directories-git
+  c $(fd -t d | fzf)
 end
 function find-directories-all
-  c $(fd -I -H -t d . ($argv | string collect) | fzf)
+  c $(fd -I -H -t d | fzf)
 end
 
 # abbreviation for fd command
@@ -12,4 +15,5 @@ abbr --add find fd
 # abbreviations
 abbr --add fd find-directories
 abbr --add fda find-directories-all
+abbr --add fdg find-directories-git
 
