1c5e0f94560854493ec3d1f49317ae0f1daf89af
Christoffer, Marschner <christoffer.marschner@sew-eurodrive.de>
2024-12-09 16:48:57 +0100

    removed zellij

--- conf.d/zellij.fish
+++
@@ -1,8 +0,0 @@
-
-abbr --add z "zellij"
-abbr --add za "zellij attach"
-abbr --add zl "zellij list-sessions"
-abbr --add zk "zellij kill-session"
-abbr --add zka "zellij kill-all-sessions"
-abbr --add zd "zellij delete-all-sessions"
-
